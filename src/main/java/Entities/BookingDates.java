e486444ee66396c2af8c42526835f87781c497b7
Kyndrael <jonathansilveira95@hotmail.com>
2025-06-05 09:49:43 -0300

    Create BookingDates.java

---
+++ src/main/java/Entities/BookingDates.java
@@ -0,0 +1,33 @@
+package entities;
+
+import java.io.Serializable;
+
+/**
+ * Representa as datas de check-in e check-out de uma reserva.
+ */
+public class BookingDates implements Serializable {
+
+    private String checkin;
+    private String checkout;
+
+    public BookingDates(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+}
